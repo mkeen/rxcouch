04cfd1df34724582ad36159f24c3e94558e5d39b
Mike Keen <mkeen.atl@gmail.com>
2018-07-23 19:40:41 -0400

    Updates

--- src/couchwatcher.ts
+++ src/couchwatcher.ts
@@ -1,4 +1,5 @@
 import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
+import { take, flatMap, map, filter, mergeAll } from 'rxjs/operators';
 import { HttpRequest } from '@mkeen/rxhttp';
 
 interface CouchDBChange {
@@ -16,19 +17,34 @@ interface CouchDBDocument {
   "_id": string;
 }
 
+interface CouchDBDesignView {
+  total_rows: number;
+  offset: number;
+  rows: CouchDBDocument[];
+}
+
 class CouchDBDocumentCollection {
   private documents: any = {};
+  public ids: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
   public add(document: CouchDBDocument) {
-    return this.documents[document._id] = new BehaviorSubject<CouchDBDocument>(document);
+    this.documents[document._id] = new BehaviorSubject<CouchDBDocument>(document);
+    this.ids
+      .pipe(take(1))
+      .subscribe((ids) => {
+        ids.push(document._id);
+        this.ids.next(ids)
+      });
+
+    return this.documents[document._id];
   }
 
   public get(id: string): BehaviorSubject<CouchDBDocument> {
     return this.documents[id];
   }
 
-  public set(document: CouchDBDocument): BehaviorSubject<CouchDBDocument> {
-    const doc = this.get(document._id);
+  public set(document: any): BehaviorSubject<CouchDBDocument> {
+    const doc = this.get(document['_id']);
     if (doc !== undefined) {
       doc.next(document);
       return doc;
@@ -45,55 +61,89 @@ export class CouchWatcher {
   private database_name: BehaviorSubject<string> = new BehaviorSubject<string>('');
   private host: BehaviorSubject<string> = new BehaviorSubject<string>('127.0.0.1');
   private port: BehaviorSubject<number> = new BehaviorSubject<number>(5984);
-  private watch: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   public documents: CouchDBDocumentCollection = new CouchDBDocumentCollection();
   private connection: any;
 
-  constructor(host: string, port: number, database_name: string, document_ids: string[] = [], watch: boolean = true) {
-    this.document_ids.next(document_ids);
+  constructor(host: string, port: number, database_name: string) {
     this.database_name.next(database_name);
     this.port.next(port);
     this.host.next(host);
-    this.watch.next(watch);
 
-    combineLatest(this.document_ids, this.database_name, this.host, this.port, this.watch)
-      .subscribe((config: any[]) => {
+    this.documents.ids
+      .pipe(filter(ids => ids !== null))
+      .subscribe(ids => this.document_ids.next(ids));
+
+    this.config()
+      .subscribe((config: [string[], string, string, number]) => {
         if (this.connection) {
           this.connection.cancel();
         }
 
         config[0].forEach((document_id: string) => {
-          this.documents.set({ '_id': document_id });
+          if (!this.documents.get(document_id)) {
+            this.documents.set({ '_id': document_id });
+          }
+
         });
 
-        if (config[4]) {
-          this.connection = new HttpRequest<CouchDBChanges>(
-            this.urlFromConfig(config), {
-              method: 'POST',
-              body: JSON.stringify({
-                'doc_ids': config[0]
-              })
+        console.log(config);
 
-            }
+        this.connection = new HttpRequest<CouchDBChanges>(
+          this.watchUrlFromConfig(config), {
+            method: 'POST',
+            body: JSON.stringify({
+              'doc_ids': config[0]
+            })
 
-          );
+          }
 
-          this.connection.send().subscribe(
-            (update: CouchDBChanges) => this.documents.set(update.doc)
-          );
+        );
 
-        }
+        this.connection.send().subscribe(
+          (update: CouchDBChanges) => {
+            console.log("update", update);
+            return this.documents.set(update.doc)
+          }
+
+        );
 
       });
 
+  }
+
+  public view(designName: string, viewName: string): Observable<CouchDBDesignView> {
+    return this.config()
+      .pipe(take(1))
+      .pipe(map((config: [string[], string, string, number]) => {
+        return (new HttpRequest<CouchDBDesignView>(
+          this.viewUrlFromConfig(config, designName, viewName), {
+            method: 'GET'
+          }
+
+        )).send();
+      }))
+      .pipe(mergeAll())
+      .pipe(take(1));
+  }
+
+  private config(): Observable<[string[], string, string, number]> {
+    return combineLatest(this.document_ids, this.database_name, this.host, this.port)
+  }
+
+  private watchUrlFromConfig(config: [string[], string, string, number]): string {
+    return `${this.urlPrefixFromConfig(config)}/${this.changeOptions()}`;
+  }
+
+  private viewUrlFromConfig(config: [string[], string, string, number], designName: string, viewName: string): string {
+    return `${this.urlPrefixFromConfig(config)}/_design/${designName}/_view/${viewName}`;
   }
 
   private changeOptions(): string {
     return '_changes?include_docs=true&feed=continuous&filter=_doc_ids';
   }
 
-  private urlFromConfig(config: any): string {
-    return `http://${config[2]}:${config[3]}/${config[1]}/${this.changeOptions()}`;
+  private urlPrefixFromConfig(config: [string[], string, string, number]): string {
+    return `http://${config[2]}:${config[3]}/${config[1]}`
   }
 
 }
