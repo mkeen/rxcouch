6d7ac32e1358923d16279f320fe1020115117232
Mike Keen <mwk@mikekeen.com>
2020-02-11 12:47:37 -0500

    fix issue with initial config, add new sugar method

--- src/sugar.ts
+++ src/sugar.ts
@@ -1,3 +1,9 @@
+import { BehaviorSubject } from 'rxjs';
+
 export function entityOrDefault(configEntity: any, defaultValue: any) {
-  return configEntity ? configEntity : defaultValue;
+  return configEntity !== undefined ? configEntity : defaultValue;
+}
+
+export function nextIfChanged(behaviorSubject: BehaviorSubject<any>, latestValue: any): void {
+  latestValue !== undefined && behaviorSubject.value !== latestValue ? behaviorSubject.next(latestValue) : null;
 }
